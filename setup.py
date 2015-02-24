ba46c393f3479b8ec98993ff5095322c03d890ea
Tomasz Czyż <tomasz.czyz@gmail.com>
2015-02-24 20:44:14 +0000

    release 0.5.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class Tox(TestCommand):
 
 setup(
     name='sphinxcontrib-images',
-    version='0.4.3',
+    version='0.5.0',
     url='https://github.com/spinus/sphinxcontrib-images',
     download_url='https://pypi.python.org/pypi/sphinxcontrib-images',
     license='Apache 2',
--- sphinxcontrib/images.py
+++ sphinxcontrib/images.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __author__ = 'Tomasz Czyż <tomaszczyz@gmail.com>'
 __license__ = "Apache 2"
 
