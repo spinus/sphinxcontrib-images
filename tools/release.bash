b45a190e504b8d29d74b161563aceee04064cf6e
Tomasz Czyż <tomasz.czyz@gmail.com>
2016-11-23 23:29:44 +0000

    release script uses nix-shell now

--- tools/release.bash
+++ tools/release.bash
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash --pure -p python27 python34 python35 python36 python27Packages.virtualenv python34Packages.virtualenv python35Packages.virtualenv python36Packages.virtualenv
 set -eux
 
 virtualenv build/venv
@@ -7,14 +8,17 @@ source build/venv/bin/activate
 set -u
 
 build/venv/bin/python setup.py install
+build/venv/bin/pip install sphinx-rtd-theme
 make -C docs html
 build/venv/bin/python setup.py test
 
-for p in python2.7 python3.4
+for p in python2.7 python3.4 python3.5 python3.6
 do
   virtualenv build/venv-$p/ --python $p
   build/venv-$p/bin/pip install -U pip wheel
   build/venv-$p/bin/python setup.py bdist_wheel upload -s
 done
+echo "Upload files?..."
+read OK
 build/venv/bin/python setup.py sdist upload -s
 build/venv/bin/python setup.py upload_docs --upload-dir docs/build/html/
